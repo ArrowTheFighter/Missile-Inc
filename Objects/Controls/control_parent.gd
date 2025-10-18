25feecf761a077db4f6f531ee56117682a7d25d4
Leon <kauyonkais@outlook.de>
2025-10-18 17:51:38 +0200

    Lever sound

---
+++ Objects/Controls/control_parent.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var offset_target:Marker3D
+
+func _ready() -> void:
+	_hide_controls()
+
+func _hide_controls() -> void:
+	var offset = offset_target.global_position
+	for child in get_children():
+		child.global_position += offset
+
+func _move_in_controls() -> void:
+	var offset = offset_target.global_position
+	var tween = get_tree().create_tween()
+	for child in get_children():
+		tween.tween_property(child, "global_position", child.global_position - offset, .8).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+
+func _on_start_pressed() -> void:
+	_move_in_controls()
