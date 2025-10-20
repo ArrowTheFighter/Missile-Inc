d2fafd570e745d9c2b3752997075c6166d49abb7
Arrow <arrowthefighter@gmail.com>
2025-10-20 21:17:45 +0200

    added explosion sound

--- Objects/Bullets/ExplodingBullet/exploding_bullet_script.gd
+++ Objects/Bullets/ExplodingBullet/exploding_bullet_script.gd
@@ -2,12 +2,16 @@ extends Bullet_Base
 
 @export var Explosion_Area : Area3D
 var tracked_areas : Array[Area3D]
+var death_sound_stream = preload("uid://bthc58y84k1st")
+var death_sound
 
 func _ready() -> void:
 	super()
 	Explosion_Area.area_entered.connect(Explosion_Area_Entered)
 	Explosion_Area.area_exited.connect(Explosion_Area_Exited)
 	HitEnemy.connect(explode)
+	
+	_prepare_death_sound()
 
 func _move_bullet(delta):
 	global_translate(transform.basis.y * speed * delta)
@@ -19,6 +23,14 @@ func _process(delta: float) -> void:
 	if((current_pos - target_pos).length() < 0.05):
 		explode()
 		
+func _prepare_death_sound()->void:
+	if not death_sound_stream: return
+	death_sound = AudioStreamPlayer.new()
+	death_sound.stream = death_sound_stream
+	death_sound.name = "DeathSound"
+	death_sound.volume_db = -15
+	get_tree().root.call_deferred("add_child", death_sound)
+		
 func explode():
 	for area in tracked_areas:
 		if area.get_parent() != null:
@@ -28,6 +40,9 @@ func explode():
 		var instanced_death_particle = death_particle.instantiate()
 		get_tree().root.add_child(instanced_death_particle)
 		instanced_death_particle.global_position = global_position
+	if death_sound:
+		death_sound.play()
+		death_sound.finished.connect(func():death_sound.queue_free())
 	queue_free()
 		
 func Explosion_Area_Entered(area : Area3D):
